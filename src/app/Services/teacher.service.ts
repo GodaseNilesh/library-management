e1a533979a0d29fb752624c1d998d399bee6d979
Nilesh Godase <nileshgodase@gmail.com>
2025-06-17 23:26:49 +0530

    teacher api integration done for add, update, delete and details

---
+++ src/app/Services/teacher.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ApplicationService } from './application.service';
+import { environment } from '../environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeacherService {
+  constructor(private application: ApplicationService) {}
+
+  saveTeacher(teacher: any) {
+    let url = environment.apiUrl + '/Teacher';
+    return this.application.postData(url, teacher);
+  }
+
+  getAllTeachers() {
+    let url = environment.apiUrl + '/Teacher';
+    return this.application.getData(url);
+  }
+  getTeacherById(id: string) {
+    let url = environment.apiUrl + `/Teacher/${id}`;
+    return this.application.getData(url);
+  }
+  updateTeacherById(teacher: any) {
+    let url = environment.apiUrl + `/Teacher/${teacher.teacherId}`;
+    return this.application.putData(url, teacher);
+  }
+  deleteTeacherById(id: string) {
+    let url = environment.apiUrl + `/Teacher/${id}`;
+    return this.application.deleteData(url);
+  }
+}
--- src/app/components/Teachers/teachers-routing.module.ts
+++ src/app/components/Teachers/teachers-routing.module.ts
@@ -3,21 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { TeacherListComponent } from './teacher-list/teacher-list.component';
 import { CreateTeacherComponent } from './create-teacher/create-teacher.component';
 import { TeacherDetailsComponent } from './teacher-details/teacher-details.component';
+import { authGuardGuard } from 'src/app/auth-guards/auth-guard.guard';
 
 const routes: Routes = [
   {
+    canActivate: [authGuardGuard],
     path:'',
     component:TeacherListComponent
   },
   {
+    canActivate: [authGuardGuard],
     path:'create-teacher',
     component:CreateTeacherComponent
   },
   {
+    canActivate: [authGuardGuard],
     path:'create-teacher/:id',
     component:CreateTeacherComponent
   },
   {
+    canActivate: [authGuardGuard],
     path:'teacher-details/:id',
     component:TeacherDetailsComponent
   }
