861de0942cacaac2a44ec0db84fef040acbd9953
Nilesh Godase <nileshgodase@gmail.com>
2025-06-27 09:09:40 +0530

    books-crud-api-integration

---
+++ src/app/Services/book.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environment';
+import { ApplicationService } from './application.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  constructor(private application: ApplicationService) {}
+
+  saveBook(bookDetails: any) {
+    let url = environment.apiUrl + '/Book';
+    return this.application.postData(url, bookDetails);
+  }
+  getAllBooks() {
+    let url = environment.apiUrl + '/Book';
+    return this.application.getData(url);
+  }
+  getBookDetailsById(id: string) {
+    let url = environment.apiUrl + `/Book/${id}`;
+    return this.application.getData(url);
+  }
+  updateBookById(bookDetails: any) {
+    let url = environment.apiUrl + `/Book/${bookDetails.bookId}`;
+    return this.application.putData(url, bookDetails);
+  }
+  deleteBookById(id: string) {
+    let url = environment.apiUrl + `/Book/${id}`;
+    return this.application.deleteData(url);
+  }
+}
--- src/app/components/books/book-routing.module.ts
+++ src/app/components/books/book-routing.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { AddBooksComponent } from './add-books/add-books.component';
+import { authGuardGuard } from 'src/app/auth-guards/auth-guard.guard';
 
 const routes: Routes = [
   {
+    canActivate: [authGuardGuard],
     path:'',
     component:BookListComponent
   },
   {
+    canActivate: [authGuardGuard],
     path:'add-book',
     component:AddBooksComponent
   },
   {
+    canActivate: [authGuardGuard],
     path:'add-book/:id',
     component:AddBooksComponent
   }
